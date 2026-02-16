0ab9aec1994c5b9386d6fcd8e29d9f69dcbd3fe7
hoyoboy0726123 <wilson_bai@me.com>
2026-02-16 13:27:36 +0800

    Deploy BYOK version to new repository

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/3d-nebula-name-picker/',
+      base: '/3D-Nebula-Name-Picker-BYOK/',
       server: {
         port: 3000,
         host: '0.0.0.0',
